dcd113ab4123967ee9c370c326adb36f0137faf5
viniciussantos <viniciussantos@hotmail.com>
2020-04-01 20:46:40 -0300

    Inclusão do projeteto teste ODM ActivosFijos

---
+++ workspace/ActivosFijosDefinition/src/ComponentesPep.java
@@ -0,0 +1,243 @@
+
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class ComponentesPep {
+
+	String pepOrigen;
+	BigDecimal porcentajeNormaLiquidacion;
+	String ordenInversion;
+	Date fechaActaAceptacionFechaEntregaAreaProductiva;
+	Date fechaPuestaFuncionamiento;
+	String tipoActivo;
+	String numeroActivoAntiguo;
+	Integer claseActivosFijos;
+	String descripcionClaseActivosFijos;
+	Integer uma;
+	Integer grupoClaseActivosFijos;
+	Integer superanumero;
+	Integer cecoDepreciacion;
+	String descipcionCeco; //Confirmar Nome da Variavel
+	String emplazamiento;
+	String descripcionEmplazamiento;
+	String local;
+	String denominacionActivoFijo1;
+	String denominacionActivoFijo2;
+	String marcaModelo;
+	String numeroSerie;
+	String unidadMedida;
+	Integer unidadesPorActivo;
+	String centro;
+	String matriculaVehiculo; //Variavel com acento
+	String codigoGGI;
+	String codigoProveedor;
+	String fabricante;
+	Integer dePeriodo;
+	Integer deEjercicio;
+	Integer aPeriodo;
+	Integer aEjercicio;
+	String observacionActivoFijo;
+	public String getPepOrigen() {
+		return pepOrigen;
+	}
+	public void setPepOrigen(String pepOrigen) {
+		this.pepOrigen = pepOrigen;
+	}
+	public BigDecimal getPorcentajeNormaLiquidacion() {
+		return porcentajeNormaLiquidacion;
+	}
+	public void setPorcentajeNormaLiquidacion(BigDecimal porcentajeNormaLiquidacion) {
+		this.porcentajeNormaLiquidacion = porcentajeNormaLiquidacion;
+	}
+	public String getOrdenInversion() {
+		return ordenInversion;
+	}
+	public void setOrdenInversion(String ordenInversion) {
+		this.ordenInversion = ordenInversion;
+	}
+	public Date getFechaActaAceptacionFechaEntregaAreaProductiva() {
+		return fechaActaAceptacionFechaEntregaAreaProductiva;
+	}
+	public void setFechaActaAceptacionFechaEntregaAreaProductiva(Date fechaActaAceptacionFechaEntregaAreaProductiva) {
+		this.fechaActaAceptacionFechaEntregaAreaProductiva = fechaActaAceptacionFechaEntregaAreaProductiva;
+	}
+	public Date getFechaPuestaFuncionamiento() {
+		return fechaPuestaFuncionamiento;
+	}
+	public void setFechaPuestaFuncionamiento(Date fechaPuestaFuncionamiento) {
+		this.fechaPuestaFuncionamiento = fechaPuestaFuncionamiento;
+	}
+	public String getTipoActivo() {
+		return tipoActivo;
+	}
+	public void setTipoActivo(String tipoActivo) {
+		this.tipoActivo = tipoActivo;
+	}
+	public String getNumeroActivoAntiguo() {
+		return numeroActivoAntiguo;
+	}
+	public void setNumeroActivoAntiguo(String numeroActivoAntiguo) {
+		this.numeroActivoAntiguo = numeroActivoAntiguo;
+	}
+	public Integer getClaseActivosFijos() {
+		return claseActivosFijos;
+	}
+	public void setClaseActivosFijos(Integer claseActivosFijos) {
+		this.claseActivosFijos = claseActivosFijos;
+	}
+	public String getDescripcionClaseActivosFijos() {
+		return descripcionClaseActivosFijos;
+	}
+	public void setDescripcionClaseActivosFijos(String descripcionClaseActivosFijos) {
+		this.descripcionClaseActivosFijos = descripcionClaseActivosFijos;
+	}
+	public Integer getUma() {
+		return uma;
+	}
+	public void setUma(Integer uma) {
+		this.uma = uma;
+	}
+	public Integer getGrupoClaseActivosFijos() {
+		return grupoClaseActivosFijos;
+	}
+	public void setGrupoClaseActivosFijos(Integer grupoClaseActivosFijos) {
+		this.grupoClaseActivosFijos = grupoClaseActivosFijos;
+	}
+	public Integer getSuperanumero() {
+		return superanumero;
+	}
+	public void setSuperanumero(Integer superanumero) {
+		this.superanumero = superanumero;
+	}
+	public Integer getCecoDepreciacion() {
+		return cecoDepreciacion;
+	}
+	public void setCecoDepreciacion(Integer cecoDepreciacion) {
+		this.cecoDepreciacion = cecoDepreciacion;
+	}
+	public String getDescipcionCeco() {
+		return descipcionCeco;
+	}
+	public void setDescipcionCeco(String descipcionCeco) {
+		this.descipcionCeco = descipcionCeco;
+	}
+	public String getEmplazamiento() {
+		return emplazamiento;
+	}
+	public void setEmplazamiento(String emplazamiento) {
+		this.emplazamiento = emplazamiento;
+	}
+	public String getDescripcionEmplazamiento() {
+		return descripcionEmplazamiento;
+	}
+	public void setDescripcionEmplazamiento(String descripcionEmplazamiento) {
+		this.descripcionEmplazamiento = descripcionEmplazamiento;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public String getDenominacionActivoFijo1() {
+		return denominacionActivoFijo1;
+	}
+	public void setDenominacionActivoFijo1(String denominacionActivoFijo1) {
+		this.denominacionActivoFijo1 = denominacionActivoFijo1;
+	}
+	public String getDenominacionActivoFijo2() {
+		return denominacionActivoFijo2;
+	}
+	public void setDenominacionActivoFijo2(String denominacionActivoFijo2) {
+		this.denominacionActivoFijo2 = denominacionActivoFijo2;
+	}
+	public String getMarcaModelo() {
+		return marcaModelo;
+	}
+	public void setMarcaModelo(String marcaModelo) {
+		this.marcaModelo = marcaModelo;
+	}
+	public String getNumeroSerie() {
+		return numeroSerie;
+	}
+	public void setNumeroSerie(String numeroSerie) {
+		this.numeroSerie = numeroSerie;
+	}
+	public String getUnidadMedida() {
+		return unidadMedida;
+	}
+	public void setUnidadMedida(String unidadMedida) {
+		this.unidadMedida = unidadMedida;
+	}
+	public Integer getUnidadesPorActivo() {
+		return unidadesPorActivo;
+	}
+	public void setUnidadesPorActivo(Integer unidadesPorActivo) {
+		this.unidadesPorActivo = unidadesPorActivo;
+	}
+	public String getCentro() {
+		return centro;
+	}
+	public void setCentro(String centro) {
+		this.centro = centro;
+	}
+	public String getMatriculaVehiculo() {
+		return matriculaVehiculo;
+	}
+	public void setMatriculaVehiculo(String matriculaVehiculo) {
+		this.matriculaVehiculo = matriculaVehiculo;
+	}
+	public String getCodigoGGI() {
+		return codigoGGI;
+	}
+	public void setCodigoGGI(String codigoGGI) {
+		this.codigoGGI = codigoGGI;
+	}
+	public String getCodigoProveedor() {
+		return codigoProveedor;
+	}
+	public void setCodigoProveedor(String codigoProveedor) {
+		this.codigoProveedor = codigoProveedor;
+	}
+	public String getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+	public Integer getDePeriodo() {
+		return dePeriodo;
+	}
+	public void setDePeriodo(Integer dePeriodo) {
+		this.dePeriodo = dePeriodo;
+	}
+	public Integer getDeEjercicio() {
+		return deEjercicio;
+	}
+	public void setDeEjercicio(Integer deEjercicio) {
+		this.deEjercicio = deEjercicio;
+	}
+	public Integer getaPeriodo() {
+		return aPeriodo;
+	}
+	public void setaPeriodo(Integer aPeriodo) {
+		this.aPeriodo = aPeriodo;
+	}
+	public Integer getaEjercicio() {
+		return aEjercicio;
+	}
+	public void setaEjercicio(Integer aEjercicio) {
+		this.aEjercicio = aEjercicio;
+	}
+	public String getObservacionActivoFijo() {
+		return observacionActivoFijo;
+	}
+	public void setObservacionActivoFijo(String observacionActivoFijo) {
+		this.observacionActivoFijo = observacionActivoFijo;
+	}	
+	
+	
+	
+	
+}
---
+++ workspace/ActivosFijosDefinition/src/SolicitacionPep.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+
+
+/**
+ * @author viniciussantos
+ *
+ */
+ 
+
+
+public class SolicitacionPep {
+
+	Boolean portalCliente;
+	Boolean esProyectosEspeciales;
+	Boolean pepBlqueada;
+	ComponentesPep[] componentePep;	
+
+	public Boolean getPortalCliente() {
+		return portalCliente;
+	}
+
+	public void setPortalCliente(Boolean portalCliente) {
+		this.portalCliente = portalCliente;
+	}
+
+	public Boolean getEsProyectosEspeciales() {
+		return esProyectosEspeciales;
+	}
+
+	public void setEsProyectosEspeciales(Boolean esProyectosEspeciales) {
+		this.esProyectosEspeciales = esProyectosEspeciales;
+	}
+
+	public Boolean getPepBlqueada() {
+		return pepBlqueada;
+	}
+
+	public void setPepBlqueada(Boolean pepBlqueada) {
+		this.pepBlqueada = pepBlqueada;
+	}
+
+	public ComponentesPep[] getComponentePep() {
+		return componentePep;
+	}
+
+	public void setComponentePep(ComponentesPep[] componentePep) {
+		this.componentePep = componentePep;
+	}
+
+
+	
+}
